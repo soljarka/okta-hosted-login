aa5792281ff5995f798aa9fbaf4a64a60dfc0800
Dmitry Maximov <Dmitry_Maximov@rcomext.com>
2018-10-02 18:24:46 +0200

    redirect_uri va env vars

--- src/app/.samples.config.ts
+++ src/app/.samples.config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oagfyv9jseOXg0oP0h7',
     issuer: 'https://identity-np.swissre.com/oauth2/ausggbrfgxYHfA9FH0h7',
-    redirectUri: 'http://spa.ac-np.swissre.com/implicit/callback',
+    redirectUri: process.env.REDIRECT_URI || 'http://spa.ac-np.swissre.com/implicit/callback',
     scope: 'openid',
   },
   resourceServer: {

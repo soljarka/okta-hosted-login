a3688cfaf51d747cafc9ea1c6d1019b792519ac6
Andrey Strizhevsky <Andrey_Strizhevsky@rcomext.com>
2018-10-02 16:06:26 +0300

    First commit

---
+++ src/app/.samples.config.ts
@@ -0,0 +1,11 @@
+export default {
+  oidc: {
+    clientId: '0oagfyv9jseOXg0oP0h7',
+    issuer: 'https://identity-np.swissre.com/oauth2/ausggbrfgxYHfA9FH0h7',
+    redirectUri: 'http://localhost:8080/implicit/callback',
+    scope: 'openid',
+  },
+  resourceServer: {
+    messagesUrl: 'https://go-for-beer.ac-np.swissre.com/secured/beers',
+  },
+};
